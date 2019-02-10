88b4331bf108547b9adb277c8851ddb1c9f00a01
hoodielive <hoodielive@gmail.com>
2019-02-10 16:17:19 -0500

    added another pipe

--- v6/make-sudoku-playable.pl
+++ v6/make-sudoku-playable.pl
@@ -5,10 +5,7 @@ my $sudoku = '000000075000080094000500600010000200000900057006003040001000023080
 $sudoku = $sudoku.trans('0' => ' '); # trans replaces each occurence of the pairs key with pairs value and returns
                                 # the resulting string.
 
-#for 0..8 -> $line-number {
-#    say substr $sudoku, $line-number * 9, 9;
-#}
-
+# sub is a subroutine
 sub chunks(Str $s, Int $chars) {
     gather loop (my $idx = 0; $idx < $s.chars; $idx += $chars) {
         take substr($s, $idx, $chars)
@@ -16,5 +13,5 @@ sub chunks(Str $s, Int $chars) {
 }
 
 for chunks($sudoku, 9) -> $line {
-    say chunks($line, 3).join('|');
+    say '|', chunks($line, 3).join('|');
 }
